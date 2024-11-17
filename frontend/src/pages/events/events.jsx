c4d771790ebf79bc35c13a97128a43e696810346
geli4njam <chesnanwoke@gmail.com>
2024-11-17 17:02:25 -0600

    Added basic layout

--- frontend/src/pages/events/events.jsx
+++ frontend/src/pages/events/events.jsx
@@ -3,7 +3,33 @@ import './events.css';
 export default function Events() {
     return (
         <div className="events">
-            <h1 className="events-header">Events - WIP</h1>
+            <h1 className="events-header">Events</h1>
+            <p className='test'>Weekly Events hosted by TAMU ColorStack </p>
+            <div className='events-container'>
+                <div className='sunday-event'>
+                    <h1 className='day'>Sunday </h1>
+                    <img className='day-image' src="" alt="Image could not be found." />
+                </div>
+                <div className='monday-event'>
+                    <h1 className='day'>Monday </h1>
+                </div>
+                <div className='tuesday-event'>
+                    <h1 className='day'>Tuesday </h1>
+                </div>
+                <div className='wednesday-event'>
+                    <h1 className='day'>Wednesday </h1>
+                </div>
+                <div className='thurday-event'>
+                    <h1 className='day'>Thursday </h1>
+                </div>
+                <div className='friday-event'>
+                    <h1 className='day'>Friday </h1>
+                </div>
+                <div className='saturday-event'>
+                    <h1 className='day'>Saturday </h1>
+                </div>
+            </div>
+
         </div>
     )
 }
