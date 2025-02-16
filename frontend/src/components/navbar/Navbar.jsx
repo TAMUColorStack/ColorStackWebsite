9eb1cc236e3a4499c7f90bda7facc950983a9dcb
Adam Vasquez <adamv0629@tamu.edu>
2025-02-16 16:51:10 -0600

    Added navbar navigation

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,6 +1,18 @@
 import './Navbar.css'
-
+import { useNavigate} from 'react-router-dom'
 export default function Navbar(){
+    const navigate = useNavigate()
+    const goToHome = () => {
+        navigate('/')
+    }
+
+    const goToAbout = () =>{
+        navigate('/about')
+    }
+
+    const goToSignIn = () => {
+        navigate('/sign_in')
+    }
 
     return (
         <>
@@ -9,9 +21,9 @@ export default function Navbar(){
                 <img src="/tamucolorstacklogo.png" alt="tamucolorstack logo" />
             </div>
             <ul class="navbar-links">
-                <li><a href="#Home.jsx">Home</a></li>
-                <li><a href="#About.jsx">About us</a></li>
-                <li><a href="#Signin.jsx">Sign in</a></li>
+                <li><a type="button" onClick={ goToHome}>Home</a></li>
+                <li><a type="button" onClick={ goToAbout}>About</a></li>
+                <li><a type="button" onClick={ goToSignIn}>Sign In</a></li>
             </ul>
         </nav>
         </>
