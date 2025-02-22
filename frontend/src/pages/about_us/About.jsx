2cb2209311a8973322f6cd9483a19a9a61406f0e
Adam Vasquez <adamv0629@tamu.edu>
2025-02-21 18:15:30 -0600

    Fixed vertical scrollbar issue in homepage, created about page mission statement, and created signin page

--- frontend/src/pages/about_us/About.jsx
+++ frontend/src/pages/about_us/About.jsx
@@ -2,7 +2,15 @@ import './About.css'
 
 export default function About(){
     return (
-        <>
+        <>  
+            <div className="about-mission-container">
+                <h2 className="mission-statement-header">Our Mission</h2>
+                <p className="mission-statement-subtext">
+                ColorStack TAMU is committed to empowering Black, Latinx, and Indigenous technologists to graduate and thrive
+                in technical careers. Through tailored resources like resume workshops, networking events, and interview prep,
+                we foster an inclusive campus environment that opens doors and builds a more diverse tech industry.
+                </p>
+            </div>
         </>
     )
 }
