f3c91829e43d411566d61b809f2eb2b4c43bff34
Hussam Makhoul <hussammakhoul2733@gmail.com>
2025-03-02 16:17:24 -0600

    Updated Cards

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -16,11 +16,11 @@ export default function Navbar(){
 
     return (
         <>
-        <nav class="navbar">
-            <div class="logo">
+        <nav className="navbar">
+            <div className="logo">
                 <img src="/tamucolorstacklogo.png" alt="tamucolorstack logo" />
             </div>
-            <ul class="navbar-links">
+            <ul className="navbar-links">
                 <li><a type="button" onClick={ goToHome}>Home</a></li>
                 <li><a type="button" onClick={ goToAbout}>About</a></li>
                 <li><a type="button" onClick={ goToSignIn}>Sign In</a></li>
