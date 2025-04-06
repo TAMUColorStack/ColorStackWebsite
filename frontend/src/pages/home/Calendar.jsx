3969a7fae77e775465d695b0cd3814ffdded4128
TAMU ColorStack <157253256+TAMUColorStack@users.noreply.github.com>
2025-04-06 21:56:30 +0000

    updated calendar link

--- frontend/src/pages/home/Calendar.jsx
+++ frontend/src/pages/home/Calendar.jsx
@@ -7,7 +7,7 @@ export default function Calendar() {
       <h2 className="calendar-header">Upcoming Events</h2>
       <iframe
         title="Google Calendar"
-        src="https://calendar.google.com/calendar/embed?src=c_aabbb026d8177a86cec45ba94d5fc10e9150377ace819fc11effa2933672d04b%40group.calendar.google.com&ctz=America/Chicago"
+        src="https://calendar.google.com/calendar/embed?src=tamucolorstack%40gmail.com&ctz=America%2FChicago"
         style={{ border: 0 }}
         width="800"
         height="600"
